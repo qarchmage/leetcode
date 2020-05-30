540875e0014f8e10ff02d95caa7f704dcdc368ed
matthias <mthilo.happ@gmail.com>
2020-05-30 15:15:11 +0200

    up

---
+++ reverse-string-344/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []string{"hello", "matthias", "LICH", "ff", "asdf"}
+	for _, v := range a {
+		b := []byte(v)
+		reverseString(b)
+		fmt.Println(string(b))
+	}
+}
+func reverseString(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+
+}
