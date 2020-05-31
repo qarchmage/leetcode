71ee15e55c953929da3a5e655902374b8f5141de
matthias <mthilo.happ@gmail.com>
2020-05-31 20:01:10 +0200

    up

---
+++ count-vowel-permutations/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 18208803
+	fmt.Println(countVowelPermutation(144))
+}
+
+func countVowelPermutation(n int) int {
+	a, e, i, o, u := 1, 1, 1, 1, 1
+	for x := 0; x < n-1; x++ {
+		a, e, i, o, u = e+i+u, a+i, e+o, i, i+o
+	}
+	return (a + e + i + o + u) % (1e9 + 7)
+}
